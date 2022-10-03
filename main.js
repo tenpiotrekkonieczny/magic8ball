ac594e751bdae07204ca8836997fa4735113acb8
Piotr Konieczny <piotrekprogramista@gmail.com>
2022-10-03 22:40:23 +0200

    adding functions for creating ball animation

--- main.js
+++ main.js
@@ -1,51 +1,53 @@
-const ball = document.querySelector('img')
-const input = document.querySelector('input')
-const answer = document.querySelector('.answer')
-const error = document.querySelector('.error')
+const ball = document.querySelector('img');
+const input = document.querySelector('input');
+const answer = document.querySelector('.answer');
+const error = document.querySelector('.error');
 
 const answersArr = [
-	'Spróbuj później',
-	'Spróbuj ponownie',
-	'Brak opinii',
-	'To twoje przeznaczenie',
-	'Kości zostały rzucone',
-	'Jedna szansa z dwóch',
-	'Zadaj pytanie ponownie',
-	'Moim zdaniem tak',
-	'Na pewno',
-	'Tak, absolutnie',
-	'Możesz na to liczyć',
-	'Możesz na to liczyć',
-	'Bardzo prawdopodobne',
-	'Tak',
-	'Na dobry początek',
-	'To nie jest',
-	'Mało prawdopodobne',
-	'Nie śnij...',
-	'Nie licz...',
-	'Niemożliwy ',
-]
+  'Spróbuj później',
+  'Spróbuj ponownie',
+  'Brak opinii',
+  'To twoje przeznaczenie',
+  'Kości zostały rzucone',
+  'Jedna szansa z dwóch',
+  'Zadaj pytanie ponownie',
+  'Moim zdaniem tak',
+  'Na pewno',
+  'Tak, absolutnie',
+  'Możesz na to liczyć',
+  'Możesz na to liczyć',
+  'Bardzo prawdopodobne',
+  'Tak',
+  'Na dobry początek',
+  'To nie jest',
+  'Mało prawdopodobne',
+  'Nie śnij...',
+  'Nie licz...',
+  'Niemożliwy ',
+];
 
+const shakeBall = () => {
+  ball.classList.add('shake-animation');
+  setTimeout(checkInput, 1000);
+};
 
 const checkInput = () => {
-    if (input.value !== '' && input.value.slice(-1) === '?') {
-        generateAnswer();
-        error.textContent = ''
-    } else if (input.value !== '' && input.value.slice(-1) !== '?') {
-        error.textContent = 'Pytanie musibyć akończone "?"'
-        answer.textContent = ''
-    } else {
-        error.textContent = 'Musisz zadać pytanie!'
-				answer.textContent = ''
-    }
-}
+  if (input.value !== '' && input.value.slice(-1) === '?') {
+    generateAnswer();
+    error.textContent = '';
+  } else if (input.value !== '' && input.value.slice(-1) !== '?') {
+    error.textContent = 'Pytanie musibyć akończone "?"';
+    answer.textContent = '';
+  } else {
+    error.textContent = 'Musisz zadać pytanie!';
+    answer.textContent = '';
+  }
+  ball.classList.remove('shake-animation');
+};
 
 const generateAnswer = () => {
+  const number = Math.floor(Math.random() * 20);
+  answer.innerHTML = `<span>Odpowiedź:</span> ${answersArr[number]}`;
+};
 
-    const number = Math.floor(Math.random() * 20)
-    answer.innerHTML = `<span>Odpowiedź:</span> ${answersArr[number]}`
-    
-}
-
-ball.addEventListener('click', checkInput)
-
+ball.addEventListener('click', shakeBall);
